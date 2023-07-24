bd5d35578d4d2ba2c53fd38f8e31bc04469b5fcc
Bokdem <ikheetmathijs@hotmail.com>
2023-07-24 20:40:20 +0200

    added postcss plugin + file ordering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles.css';
+import LeftColumn from './components/LeftColumn';
 
 function App() {
   return (
     <div className="App">
-
+      <LeftColumn />
     </div>
   );
 }
---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,18 @@
+export default function LeftColumn() {
+    return (
+        <>
+        <div className="left-column">
+            <h1 className="main-header">Mathijs de Bok</h1>
+            <h3>Front End Developer</h3>
+            <p className="main-content">I build accessible, user friendly products and digital experiences for the web.</p>
+            <div className="mini-menu">
+                <ul>
+                    <li><a href="#top">About</a></li>
+                    <li><a href="#top">Experience</a></li>
+                    <li><a href="#top">Projects</a></li>
+                    <li><a href="#top">Mini experiments</a></li>
+            </ul></div>
+        </div>
+        </>
+    )
+}
