5fb85b1101313609911eb5a573e7594ea1273f98
Bokdem <ikheetmathijs@hotmail.com>
2023-07-26 11:45:43 +0200

    Fix overscroll left column

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export default [
     {
         id: 2,
         title: "Lessons with Katie Zaferes",
-        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        description: "Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         tags: "tag1, tag2, tag3"
     },
     {
