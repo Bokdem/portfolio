434f94a72dc12f66fc8892d822b8c227e40304ce
Bokdem <ikheetmathijs@hotmail.com>
2023-08-14 11:30:46 +0200

    added project-card images and Netlify deployments

--- src/json-data/data-projects.js
+++ src/json-data/data-projects.js
@@ -3,7 +3,7 @@ export default [
         id: 4,
         dateMonths: "",
         dateYears: "",
-        thumbnail: "../../images/course-card.png",
+        thumbnail: "../../images/project-card1.png",
         title: "Notes app with Firebase",
         company: "",
         description: "With this project I got acquainted with handling the end users input. We used React-mde and showdown to build the interface. During development we used LocalStorage to store the data, after which we switched to connecting it to Firebase. This project was built by following a course I followed to level up my knowledges in React.",
@@ -14,22 +14,22 @@ export default [
         id: 5,
         dateMonths: "",
         dateYears: "",
-        thumbnail: "../../images/course-card.png",
+        thumbnail: "../../images/project-card2.png",
         title: "Pixabay API Gallery Search",
         company: "",
         description: "With this project I got acquainted with fetching an API from a third-party server, mapping through it's returning data, passing the data into the components, using a text input field in React to provide the end user with search results and more. I built this project by following an online course where the narrator mentored me through the material.",
         tags: "React, API, Axios, Javascript, Debouncing",
-        hrefLink: ""
+        hrefLink: "https://64d9ea17faf3191a6b5eed12--dapper-pavlova-e7b4b2.netlify.app/"
     },
     {
         id: 6,
         dateMonths: "",
         dateYears: "",
-        thumbnail: "../../images/course-card.png",
+        thumbnail: "../../images/project-card3.png",
         title: "To-Do List",
         company: "",
         description: "A very simple to-do webapp, to get my hands om some of the basic principles of React. You can add new to-do's, mark them as done and delete them using LocalStorage. Eventually this quickly evovled into a playground in which I try out some other aspects of React. Such as fetching API's, handling forms, ...",
         tags: "React, LocalStorage, JavaScript, Forms, API, Project playground",
-        hrefLink: ""
+        hrefLink: "https://64d9f28cd2d5de2054d9801e--teal-lamington-509695.netlify.app/"
     }
 ]
